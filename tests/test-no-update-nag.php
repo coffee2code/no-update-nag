9923dcfe869e51cb784546a2f7845ab643e5939b
Scott Reilly <scott@coffee2code.com>
2019-04-01 00:07:08 -0600

    Unit tests: Add unit test to verify `update_nag()` gets unhooked

--- tests/test-no-update-nag.php
+++ tests/test-no-update-nag.php
@@ -33,4 +33,20 @@ class No_Update_Nag_Test extends WP_UnitTestCase {
 		$this->assertEquals( 10, has_filter( 'admin_init', 'c2c_no_update_nag' ) );
 	}
 
+	public function test_unhooks_update_nag() {
+		require_once( ABSPATH . '/wp-admin/includes/admin-filters.php' );
+		require_once( ABSPATH . '/wp-admin/includes/ms-admin-filters.php' );
+
+		// Verify that update nag is potentially shown.
+		$this->assertEquals( 3, has_filter( 'admin_notices', 'update_nag' ) );
+		$this->assertEquals( 3, has_filter( 'network_admin_notices', 'update_nag' ) );
+
+		// Trigger plugin (as would be done via admin_init).
+		c2c_no_update_nag();
+
+		// Verify update nag will not be potentially shown.
+		$this->assertFalse( has_filter( 'admin_notices', 'update_nag' ) );
+		$this->assertFalse( has_filter( 'network_admin_notices', 'update_nag' ) );
+	}
+
 }
